78741acee5fa7fd987cbe5df5772577aac6fc77a
Abhinav SInha <a.s.rex2006@gmail.com>
2017-05-03 16:17:24 -0500

    Initialised class and variables,

---
+++ Agents.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy
+import matplotlib.pyplot as plt
+
+#Set display Options and seed generator
+plt.rc('font', family='serif', size=40)
+pd.set_option('display.width', 1000)
+numpy.random.seed(6845)
+
+class Agent_Generation:
+
